9a45907d16a3a91e0fdf432efbca3181cb01c183
SudhirGZB <sudhirrules11@gmail.com>
2026-02-20 15:26:42 +0530

    Update showMessage response string
    Modify RansacCtrl#showMessage to return "it's transactional msg using CI CD completed1" instead of the previous message. This is a minor text change to the endpoint's response (likely for testing/versioning purposes).

--- transactional/transactional/src/main/java/com/microservices/transac/ctrl/RansacCtrl.java
+++ transactional/transactional/src/main/java/com/microservices/transac/ctrl/RansacCtrl.java
@@ -10,7 +10,7 @@ public class RansacCtrl {
 	
 @GetMapping("/showmsg")	
 String showMessage() {
-	return "it's transactional msg using CI CD completed";
+	return "it's transactional msg using CI CD completed1";
 	
 }
 
